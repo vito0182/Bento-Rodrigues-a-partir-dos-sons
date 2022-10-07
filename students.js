5f3d12bd3cf1ae5fecaaa18c5bee0b60e5346e87
Luiz Miguel dos Santos Gonçalves <lm4academy@gmail.com>
2022-10-07 15:30:37 -0300

    description and square images

--- students.js
+++ students.js
@@ -1,54 +1,67 @@
 var students = [
     {
         "name": "Ana Beatriz Messias Gonçalves",
-        "description": "Tenho 11 anos. Eu gosto muito de andar à cavalo, inclusive tenho três. Gosto de andar de bicicleta e também de cantar e dançar."
+        "description": "Tenho 11 anos. Eu gosto muito de andar à cavalo, inclusive tenho três. Gosto de andar de bicicleta e também de cantar e dançar.",
+        "image": "ana_m.jpeg"
     },
     {
         "name": "Ana Beatriz Larri Santos",
-        "description": "Tenho 11 anos, moro com meus pais e com meu irmão. O que eu mais gosto de fazer é dançar, cantar e andar de bicicleta. Tenho 3 cachorrinhos e estou no 6º ano."
+        "description": "Tenho 11 anos, moro com meus pais e com meu irmão. O que eu mais gosto de fazer é dançar, cantar e andar de bicicleta. Tenho 3 cachorrinhos e estou no 6º ano.",
+        "image": "ana_l.png"
     },
     {
         "name": "Esther Agda dos Santos",
-        "description": "Tenho 11 anos, gosto muito de animais. Tenho 3 cachorros e 2 cavalos. Meu esporte favorito é montar cavalo. Gosto também de  jogar queimada, roubar bandeira e futebol. Gosto de vir à Escola e minhas aulas favoritas são Educação Física, História, Português, Inglês e Geografia."
+        "description": "Tenho 11 anos, gosto muito de animais. Tenho 3 cachorros e 2 cavalos. Meu esporte favorito é montar cavalo. Gosto também de  jogar queimada, roubar bandeira e futebol. Gosto de vir à Escola e minhas aulas favoritas são Educação Física, História, Português, Inglês e Geografia.",
+        "image": "esther.jpeg"
     },
     {
         "name": "Gustavo Henrique I. Santos",
-        "description": "Tenho 11 anos, moro em Mariana. Gosto de jogar futebol, andar a cavalo, gosto de animais. Tenho três irmãos, um cachorro e tenho duas éguas. Morava no Bento e gostava muito de lá e meu sonho é ir em um haras."
+        "description": "Tenho 11 anos, moro em Mariana. Gosto de jogar futebol, andar a cavalo, gosto de animais. Tenho três irmãos, um cachorro e tenho duas éguas. Morava no Bento e gostava muito de lá e meu sonho é ir em um haras.",
+        "image": "gustavo.png"
     },
     {
         "name": "Ítalo Eleutério dos Santos",
-        "description": "Eu nasci dia 17 de dezembro de 2010. Sou do 6º ano, tenho 11 anos. Minha comida favorita é strogonoff. Tenho 6 cachorros. "
+        "description": "Eu nasci dia 17 de dezembro de 2010. Sou do 6º ano, tenho 11 anos. Minha comida favorita é strogonoff. Tenho 6 cachorros. ",
+        "image": "person.png"
     },
     {
         "name": "Júlia Gabrielly Silva",
-        "description": "Tenho 11 anos. Gosto de animais, tenho um cachorro e um gato. Amo andar de bicicleta e adoro passear. Odeio ir para a Escola."
+        "description": "Tenho 11 anos. Gosto de animais, tenho um cachorro e um gato. Amo andar de bicicleta e adoro passear. Odeio ir para a Escola.",
+        "image": "julia.jpeg"
     },
     {
         "name": "Kaíque Gabriel da Silva",
-        "description": "Tenho 12 anos. Gosto de futebol. Tenho três cachorros. Estudo no 6º ano. Adoro andar de bicicleta e gosto de chocolate."
+        "description": "Tenho 12 anos. Gosto de futebol. Tenho três cachorros. Estudo no 6º ano. Adoro andar de bicicleta e gosto de chocolate.",
+        "image": "kaique.jpeg"
     },
     {
         "name": "Lara Gabriely Silva Souza",
-        "description": "Tenho 12 anos. Nasci no dia 19 de julho de 2020. Tenho 6 cachorros, 20 passarinhos, 10 galinhas e 2 galos. Tenho dois irmãos Thamara e Philipe. Gosto de jogar bola. Amo muito meus animais."
+        "description": "Tenho 12 anos. Nasci no dia 19 de julho de 2020. Tenho 6 cachorros, 20 passarinhos, 10 galinhas e 2 galos. Tenho dois irmãos Thamara e Philipe. Gosto de jogar bola. Amo muito meus animais.",
+        "image": "person.png"
     },
     {
         "name": "Letícia de Jesus E. Pinto",
-        "description": "Eu gosto de jogar bola, tenho uma cachorra. Gosto dos professores da minha Escola e eu minha turma estamos fazendo um site. Eu estou gostando muito dele. Adoro comer, gosto de sair, não gosto de andar a pé. Esse é um resumo de mim..."
+        "description": "Eu gosto de jogar bola, tenho uma cachorra. Gosto dos professores da minha Escola e eu minha turma estamos fazendo um site. Eu estou gostando muito dele. Adoro comer, gosto de sair, não gosto de andar a pé. Esse é um resumo de mim...",
+        "image": "leticia.jpeg"
     },
     {
         "name": "Maria Eduarda N. Silva",
-        "description": "Tenho 11 anos, gosto de jogar queimada, peteca. Eu estudo na Escola de Bento Rodrigues e gosto de jogar Roblox. Gosto de estudar."
+        "description": "Tenho 11 anos, gosto de jogar queimada, peteca. Eu estudo na Escola de Bento Rodrigues e gosto de jogar Roblox. Gosto de estudar.",
+        "image": "maria.jpeg"
     },
     {
         "name": "Pollyane Gomes Ferreira",
-        "description": "Tenho 11 anos. Gosto de passear. Tenho 2 cachorras, elas se chamam Luna e Susiy. Gosto de jogar futebol. Gosto muito da aula de História e de Educação Física."
+        "description": "Tenho 11 anos. Gosto de passear. Tenho 2 cachorras, elas se chamam Luna e Susiy. Gosto de jogar futebol. Gosto muito da aula de História e de Educação Física.",
+        "image": "pollyane.jpeg"
     },
     {
         "name": "Sophia Stefane de Souza",
-        "description": "Tenho 11 anos. Gosto muito de mexer no telefone e de desenhar. Gosto muito da matéria de História. E essa sou eu."
+        "description": "Tenho 11 anos. Gosto muito de mexer no telefone e de desenhar. Gosto muito da matéria de História. E essa sou eu.",
+        "image": "sophia.jpeg"
     },
     {
         "name": "Vanessa Vitória Arcanjo",
-        "description": "Tenho 11 anos e moro em Mariana – MG. Tenho uma cachorrinha chamada Jade, adoro andar de bicicleta e de cavalo, também adoro ficar perto dos meus amigos e da minha família. Eu morro de medo de aranha. Minhas matérias favoritas são Inglês, Educação Física, História e Ciências. Adoro música pop, sertaneja e jogar queimada na Escola, além de pular corda. Sou filha única."
+        "description": "Tenho 11 anos e moro em Mariana – MG. Tenho uma cachorrinha chamada Jade, adoro andar de bicicleta e de cavalo, também adoro ficar perto dos meus amigos e da minha família. Eu morro de medo de aranha. Minhas matérias favoritas são Inglês, Educação Física, História e Ciências. Adoro música pop, sertaneja e jogar queimada na Escola, além de pular corda. Sou filha única.",
+        "image": "vanessa.jpeg"
     }
 ]
